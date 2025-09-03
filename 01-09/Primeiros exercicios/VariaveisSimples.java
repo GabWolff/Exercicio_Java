38c9a08e80a92e825c53365534fcd681ea8ee300
GabWolff <gabrielbuskew@gmail.com>
2025-09-02 23:56:56 -0300

    uso de variavel simples e comandos do gitbash

---
+++ 01-09/Primeiros exercicios/VariaveisSimples.java
@@ -0,0 +1,12 @@
+
+public class VariaveisSimples {
+
+    public static void main(String[] args) {
+
+        String nome = "Gabriel";
+        int idade = 25;
+        double altura = 1.87;
+
+        System.out.println("Olá, meu nome é " + nome + ",tenho " + idade + " anos e minha altura é " + altura);
+    }
+}
